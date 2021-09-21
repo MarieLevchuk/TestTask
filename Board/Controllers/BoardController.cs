4dfbcbf3213e96fefe365958795d344a8884bef0
Мария <marie.levchuk@gmail.com>
2021-09-21 13:47:45 +0300

    added DAL, Models

---
+++ Board/Controllers/BoardController.cs
@@ -0,0 +1,73 @@
+﻿using Board.DAL;
+using Board.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Board.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BoardController : Controller
+    {
+        CardsRepository repository;
+
+        public BoardController()
+        {
+            repository = new CardsRepository();
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Card> Get()
+        {
+            return repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Card> Get(int id)
+        {
+            Card card = repository.Find(id);            
+            if (card == null)
+                return NotFound();
+            return new ObjectResult(card);
+        }
+
+        [HttpPost]
+        public ActionResult<Card> Post(Card card)
+        {
+            if (card == null)
+            {
+                return BadRequest();
+            }
+
+            repository.Add(card);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put(Card card)
+        {
+            if (card == null)
+            {
+                return BadRequest();
+            }
+            if (!repository.GetAll().Any(x => x.Id == card.Id))
+            {
+                return NotFound();
+            }
+
+            repository.Update(card);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            repository.Remove(id);
+            return Ok();
+        }
+    }
+}
---
+++ Board/DAL/CardsRepository.cs
@@ -0,0 +1,90 @@
+﻿using Board.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Board.DAL
+{
+    public class CardsRepository : ICardsRepository
+    {
+        List<Card> cards;
+
+        public CardsRepository()
+        {
+            using (StreamReader streamReader = new StreamReader("DAL/users.json"))
+            {
+                string json = streamReader.ReadToEnd();
+                cards = JsonConvert.DeserializeObject<List<Card>>(json);
+            }
+        }
+
+        public void Add(Card card)
+        {
+            if (card == null)
+            {
+                throw new ArgumentNullException(nameof(card));
+            }
+
+            cards.Add(card);
+        }
+
+        public Card Find(int id)
+        {
+            UpdateJson();
+            Card card = cards.FirstOrDefault(x => x.Id == id);
+            if (card == null)
+            {
+                throw new ArgumentException(nameof(id));
+            }
+
+            return card;
+        }
+
+        public IEnumerable<Card> GetAll()
+        {
+            UpdateJson();
+
+            return cards;
+        }
+
+        public void Remove(int id)
+        {
+            var index = cards.FindIndex(x => x.Id == id);
+            if (!cards.Any(x => x.Id == id))
+            {
+                throw new ArgumentException(nameof(id));
+            }
+
+            cards.RemoveAt(index);
+        }
+
+        public void Update(Card card)
+        {
+            if (card == null)
+            {
+                throw new ArgumentNullException(nameof(card));
+            }
+            if (!cards.Any(x => x.Id == card.Id))
+            {
+                throw new ArgumentException(nameof(card));
+            }
+
+            var index = cards.FindIndex(x => x.Id == card.Id);
+            cards[index] = card;
+        }
+
+        private void UpdateJson()
+        {
+            using (StreamWriter streamWriter = new StreamWriter("DAL/users.json"))
+            using (JsonWriter writer = new JsonTextWriter(streamWriter))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(writer, cards);
+            }
+        }
+    }
+}
+
