31c7cd54b03821047cd672b6209a22af43618489
Мария <marie.levchuk@gmail.com>
2021-09-23 15:08:27 +0300

    added ApiHelper class

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
